42b785e261dbcd5e85b09df41fca8f8906f34792
Felipe Barkokébas <jfbarkokebas@gmail.com>
2023-02-06 10:16:36 -0300

    created aside bar

--- src/components/Aside/Aside.tsx
+++ src/components/Aside/Aside.tsx
@@ -1,10 +1,25 @@
-import { Container } from './styles'
-
+import logoImg from '../../assets/logo.svg'
+import { Container, Header, LogoImg, MenuItemLink, MenuContainer, Title } from './styles'
+import {
+  MdDashboard, 
+  MdArrowDownward,  
+  MdArrowUpward,   
+  MdExitToApp
+} from 'react-icons/md'
 
 const Aside = () => {
   return (
     <Container>
-      <h1>Aside</h1>
+      <Header>
+        <LogoImg src={logoImg} alt='Logo minha carteira' />
+        <Title>Minha Carteira</Title>
+      </Header>
+      <MenuContainer>
+        <MenuItemLink href='#'> <MdDashboard/>Dashboard</MenuItemLink>
+        <MenuItemLink href='#'><MdArrowUpward/> Entradas</MenuItemLink>
+        <MenuItemLink href='#'><MdArrowDownward/> Saídas</MenuItemLink>
+        <MenuItemLink href='#'><MdExitToApp/> Sair</MenuItemLink>
+      </MenuContainer>
     </Container>
   )
 }
--- src/components/Aside/styles.ts
+++ src/components/Aside/styles.ts
@@ -2,7 +2,49 @@ import styled from 'styled-components'
 
 export const Container = styled.div`
 grid-area: AS;
-color: ${props => props.theme.color.white};
 background-color:${props => props.theme.color.secondary} ;
+padding-left: 20px;
+border-right:1px solid ${props=> props.theme.color.gray};
 
+
+`
+export const Title = styled.h3`
+ color: ${props => props.theme.color.white};
+ margin-left: 10px;
+ 
+`
+
+export const Header = styled.header`
+height: 70px;
+ display: flex;
+ align-items: center;
+`
+
+export const LogoImg = styled.img`
+ height: 40px;
+ width: 40px;
+`
+
+export const MenuContainer = styled.nav`
+display: flex;
+flex-direction: column;
+ margin-top: 50px;
+
+`
+
+export const MenuItemLink = styled.a`
+ color: ${props=> props.theme.color.info};
+ transition: opacity 0.3s;
+ margin: 8px 0;
+ display: flex;
+ align-items: center;
+
+ &:hover{
+  opacity: .7;
+ }
+
+ >svg{
+  font-size: 18px;
+  margin-right: 5px;
+ }
 `
