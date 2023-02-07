d7ad5bbfaa42f92ca1c9570da60166a946384dd0
Felipe Barkokébas <jfbarkokebas@gmail.com>
2023-02-07 16:29:15 -0300

    adding filter components

--- src/pages/List/List.tsx
+++ src/pages/List/List.tsx
@@ -2,20 +2,52 @@ import React from 'react'
 import ContentHeader from '../../components/ContentHeader/ContentHeader'
 import HistoryFinanceCard from '../../components/HistoryFinaceCard/HistoryFinanceCard'
 import SelectInput from '../../components/SelectInput/SelectInput'
-import { Container, Content } from './styles'
+import { Container, Content, Filters } from './styles'
 
 const List = () => {
-  const pets =[
-    {value:'Tobby', label:'Tobby'},
-    {value:'Ally', label:'Ally'},
-    {value:'Meg', label:'Meg'},
-    {value:'Mia', label:'Mia'}, 
+  const months =[
+    {value:1, label:'Janeiro'},
+    {value:2, label:'Fevereiro'},
+    {value:3, label:'Março'},
+    {value:4, label:'Abril'},
+    {value:5, label:'Maio'},
+    {value:6, label:'Junho'},
+    {value:7, label:'Julho'},
+    {value:8, label:'Agosto'},
+    {value:9, label:'Setembro'},
+    {value:10, label:'Outubro'},
+    {value:11, label:'Novembro'},
+    {value:12, label:'Dezembro'}, 
   ]
+  const years=[
+    {value:2023, label:2023},
+    {value:2022, label:2022},
+    {value:2021, label:2021},
+    {value:2020, label:2020},
+
+  ]
+
   return (
     <Container>
       <ContentHeader title='Saídas' lineColor='#E44C4E'>
-        <SelectInput options={pets}/>
+        <SelectInput options={months}/>
+        <SelectInput options={years}/>
       </ContentHeader>
+
+      <Filters>
+        <button 
+        type="button"
+        className='tag-filter filter-recurrent'>
+          Recorrentes
+        </button>
+        
+        <button 
+        type="button"
+        className='tag-filter filter-eventual'>
+          Eventuais
+        </button>
+      </Filters>
+
       <Content>
         <HistoryFinanceCard
         tagColor='#E44C4E'
--- src/pages/List/styles.ts
+++ src/pages/List/styles.ts
@@ -6,3 +6,41 @@ export const Container = styled.div`
 export const Content = styled.div`
  
 `
+
+export const Filters = styled.div`
+ width: 100%;
+ display: flex;
+ justify-content: center;
+ margin-bottom: 30px;
+
+  .tag-filter{
+    font-size: 18px;
+    font-weight: 500;
+    background: none;
+    margin: 0 10px;
+    color: ${props => props.theme.color.white};
+    transition: all .3s;
+
+    &:hover{
+      opacity: .7;
+      transform: scale(1.03);
+    }  
+
+  }
+
+  .filter-recurrent::after{
+      content: '';
+      display: block;
+      width: 55px;
+      margin: 0 auto;
+      border-bottom: 10px solid ${props => props.theme.color.warning} ;
+    }
+
+    .filter-eventual::after{
+      content: '';
+      display: block;
+      width: 55px;
+      margin: 0 auto;
+      border-bottom: 10px solid ${props => props.theme.color.success} ;
+    }
+`
